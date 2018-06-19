d159d7e274e5902e7727de42ee0fb3c024caa12d
qiuhaoxin <502967244@qq.com>
2018-06-20 00:06:05 +0800

    commit

--- src/example/pages/detail/index.js
+++ src/example/pages/detail/index.js
@@ -351,7 +351,9 @@ class Detial extends Component{
                   </List>
 
                   <List
-
+                    renderHeader={
+                      ()=><div>test</div>
+                    }
                   >
                     <ListItem
                        arrow={'horizontal'}
